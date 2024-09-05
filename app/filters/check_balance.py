48aaf2cef23ad67fd0944a2be19a778f6eb993df
Egor1-0 <webpanda1301@gmail.com>
2024-09-05 21:52:12 +0300

    fix govna

---
+++ app/filters/check_balance.py
@@ -0,0 +1,16 @@
+from aiogram.types import Message
+from aiogram.filters import BaseFilter
+
+from app.database.queries import get_user_money
+
+class CheckMoney(BaseFilter):
+    async def __call__(self, message: Message):
+        input_data = message.text.lower().split(' ')
+        if len(input_data) >= 2:       
+            user = await get_user_money(message.from_user.id)
+            try:
+                if user.money >= int(input_data[1]):
+                    return True
+                return False
+            except:
+                return False
--- app/handlers/ores/ores_handlers.py
+++ app/handlers/ores/ores_handlers.py
@@ -5,11 +5,11 @@ from aiogram.filters import or_f
 
 from app.filters import LenInputData, DateValue
 from app.database.queries import increanse, deincreanse
-from app.middlewares import CheckEnergy
+from app.filters import CheckEnergy
 
 ores_router = Router()
 
-ores_router.message.outer_middleware(CheckEnergy())
+ores_router.message.filter(CheckEnergy())
 
 ores_router.message.filter(F.text.lower().startswith('копать'))
 
