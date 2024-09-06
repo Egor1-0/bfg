be6febed085bb1bb0fdff8d43b1d13ab362c3159
Egor1-0 <webpanda1301@gmail.com>
2024-09-06 19:29:07 +0300

    a

--- app/handlers/ores/ores_handlers_error.py
+++ app/handlers/ores/ores_handlers_error.py
@@ -4,7 +4,9 @@ from app.filters import CheckOres, CheckEnergy
 
 ores_error = Router()
 
-ores_error.message.filter(F.text.lower().startswith('дать'))
+ores_error.message.filter(F.text.lower().startswith('копать'))
+
+
 @ores_error.message(~CheckOres())
 async def error(message: Message):
     await message.answer('❌ | Не найден такой тип руды.')
